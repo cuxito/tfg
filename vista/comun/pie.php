90a0f955bd27b89c741420654d295e11c0a4d459
sergio <sergioarroyoperez.daw2020@riberadeltajo.es>
2023-01-09 23:38:07 +0100

    update

--- vista/comun/pie.php
+++ vista/comun/pie.php
@@ -1,2 +1,23 @@
-<p class="bg-secondary col-12 p-3 text-center fs-1 m-0 " >EXAMEN Junio. Sergio Arroyo</p>
+<footer class="pie">
+        <div class="foot"><img src="./assets/images/logo.png" class="logofoot"></div>
+        <div class="foot">
+            <div class="foot2">
+                <b>CONTACTO</b>
+                <br>
+                telefono: 638248280
+                <br>
+                mail: LaflordelaVida@tusFlores.com
+                <br>
+                FAX: 972348922
+            </div>
+        </div>
+        <div class="foot">
+            <div class="foot2">
+                <a href=""><img src="./assets/images/facebook.png" class="redes"></a>
+                <a href=""><img src="./assets/images/instagram.png" class="redes"></a>
+                <a href=""><img src="./assets/images/gorjeo.png" class="redes"></a>
+            </div>
+        </div>
+        
+    </footer>
    
